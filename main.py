e458081fb6e92b5c5e386234141cd148dea43b7e
mrpodato <41372461+mrpodato@users.noreply.github.com>
2019-03-04 18:22:16 +0800

    reduced crashing

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #Enter the Webhook URL in 'WEBHOOK_HERE'
 from dhooks import Webhook
-hook = Webhook('WEBHOOK_HERE')
-print ('Enter the message you would like to send')
-src = input()
-hook.send ("`" + src + "`")
+try:
+    hook = Webhook('WEBHOOK_HERE')
+    print ('Enter the message you would like to send')
+    src = input()
+    hook.send (src)
+except Exception as e:
+    print ('Please provide a valid Webhook')
+    input()
+
