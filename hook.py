6f008665dc19dbf1123401da2136401644fda753
AliveChive <41372461+AliveChive@users.noreply.github.com>
2019-10-13 21:00:18 +1100

    changes
    added support for canary and ptb webhooks

--- hook.py
+++ hook.py
@@ -5,17 +5,20 @@ from termcolor import colored
 clear = lambda: os.system('cls')
 def main():
     clear()
-    hook = input(colored("please enter webhook url: ",'yellow'))
+    hook = input(colored("please enter webhook url: ",'yellow')).replace("ptb.", "").replace("canary.", "")
+    try:
+        Webhook(hook).get_info()
+    except:
+        clear()
+        print (colored("you did not enter a valid webhook :(",'red'))
+        input()
+        main()
+    print(colored('Logged in','green'))
+    time.sleep(1)
     while True:
         clear()
         msg = input(colored('please enter a message: ','cyan'))
-        try:
-            Webhook(hook).send(msg)
-        except Exception as i:
-            clear()
-            print (colored("you did not enter a valid webhook :(",'red'))
-            input()
-            main()
+        Webhook(hook).send(msg)
         print(colored('sent!','green'))
         time.sleep(.3)
 main()
